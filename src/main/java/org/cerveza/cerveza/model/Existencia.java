fef55031b65ec3aa07890e4d8e8e50a52bb3f71f
San-43 <virgosmp.mx@gmail.com>
2025-09-07 19:43:33 -0600

    main con todo

---
+++ src/main/java/org/cerveza/cerveza/model/Existencia.java
@@ -0,0 +1,11 @@
+package org.cerveza.cerveza.model;
+
+import java.time.LocalDate;
+
+public class Existencia {
+    private Integer idExistencia;
+    private Integer idExpendio;
+    private Integer idPresentacion;
+    private Integer cantidad;
+    private LocalDate fecha;
+}
